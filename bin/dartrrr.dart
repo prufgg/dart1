bc9e5cf5a142064a9a1b485322f4d49dd98b565d
Rodion <rodion.rubets@gmail.com>
2022-08-08 20:58:34 +0300

    second task

--- bin/dartrrr.dart
+++ bin/dartrrr.dart
@@ -1,11 +1,22 @@
-void main() {
-  var firstName = 'Andrea';
-  var lastName = 'Bizzotto';
-  var age = 36;
-  var height = 1.84;
+// void main() {
+//   var firstName = 'Andrea';
+//   var lastName = 'Bizzotto';
+//   var age = 36;
+//   var height = 1.84;
 
-  print(firstName);
-  print(lastName);
-  print(age);
-  print(height);
+//   print(firstName);
+//   print(lastName);
+//   print(age);
+//   print(height);
+// }
+
+void main() {
+  final temperature = 20;
+  final value = 2;
+  final pizza = 'pizza';
+  final pasta = 'pasta';
+
+  print('The temperature is ${temperature}C');
+  print('$value plus $value makes ${value + value}');
+  print('I like $pizza and $pasta');
 }

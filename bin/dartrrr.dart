6de3ac083b2798ba2f98c75b82c6ce33e4b3c51b
Rodion <rodion.rubets@gmail.com>
2022-08-08 21:03:53 +0300

    fourth task

--- bin/dartrrr.dart
+++ bin/dartrrr.dart
@@ -10,13 +10,20 @@
 //   print(height);
 // }
 
-void main() {
-  final temperature = 20;
-  final value = 2;
-  final pizza = 'pizza';
-  final pasta = 'pasta';
+// void main() {
+//   final temperature = 20;
+//   final value = 2;
+//   final pizza = 'pizza';
+//   final pasta = 'pasta';
 
-  print('The temperature is ${temperature}C');
-  print('$value plus $value makes ${value + value}');
-  print('I like $pizza and $pasta');
+//   print('The temperature is ${temperature}C');
+//   print('$value plus $value makes ${value + value}');
+//   print('I like $pizza and $pasta');
+// }
+
+void main() {
+  String title = 'Dart course';
+  print(title);
+  print(title.toUpperCase());
+  print(title.toLowerCase());
 }
